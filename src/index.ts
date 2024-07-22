d03b9ee5ec43b0db856a94553264d87165d903b4
Andika Tirta <andikatirtax@gmail.com>
2024-07-23 03:04:17 +0700

    Add Setting for vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,9 @@
+import app from "./app";
 import EnvironmentSettings from "./utils/EnvironmentSettings";
+
 new EnvironmentSettings();
-
-import "reflect-metadata";
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import dataSource from "./config/Database";
-import router from "./routes/Routes";
-import morgan from "morgan";
-import cors from "cors";
-import errorMiddleware from "./middlewares/ErrorMiddleware";
-
-dataSource
-  .initialize()
-  .then(() => {
-    console.log("Database Connected");
-  })
-  .catch((err) => {
-    console.error("Database Connection Error: ", err);
-  });
-
-const app = express();
-const PORT = process.env.PORT;
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
-app.use("/uploads", express.static("public/assets/uploads"));
-
-app.use(cors(corsOptions));
-app.use(morgan("dev"));
-//Middleware to parse cookies
-app.use(cookieParser());
-//Middleware to parse JSON
-app.use(bodyParser.json());
-//Use the router
-app.use(router);
-//Error handling middleware
-app.use(errorMiddleware);
-
-//Default route
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
 //start server
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
